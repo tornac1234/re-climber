92f22ccacedf4033c23b168af457d5198b67d870
Matomagic <tomymarchant1@gmail.com>
2025-11-22 12:25:06 +0100

    Fixed item pick up to consider closest item to player
    Also added interact key binding

--- Assets/Scripts/ItemPickUp.cs
+++ Assets/Scripts/ItemPickUp.cs
@@ -1,10 +1,13 @@
+using NUnit.Framework.Interfaces;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class ItemPickUp : MonoBehaviour
 {
     private const string giverTag = "Giver";
     private readonly List<ItemReference> itemsAround = new ();
+    private ItemReference closestItem;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -22,9 +25,40 @@ public class ItemPickUp : MonoBehaviour
         }
     }
 
+    private void updateClosestItem()
+    {
+        ItemReference minItem = itemsAround[0];
+        float minDist = float.MaxValue;
+        foreach (ItemReference item in itemsAround)
+        {
+            float newDist = Vector2.Distance(transform.position, item.transform.position);
+            if (newDist < minDist) 
+            {
+                minItem = item;
+                minDist = newDist;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (itemsAround.Count == 0)
+        {
+            closestItem = null;
+            return;
+        }
+        updateClosestItem();
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        if (itemsAround.Count == 0) return;
+        // Method to outline object
+        if (Input.GetButton("Interact"))
+        {
+            // [Method to pick up closest item]
+            itemsAround.Remove(closestItem);
+            updateClosestItem();
+        }
     }
 }
