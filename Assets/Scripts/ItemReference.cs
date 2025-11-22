64878cf04f0f5c511967cc3e82c3bf25354f6b46
Matomagic <tomymarchant1@gmail.com>
2025-11-22 15:08:28 +0100

    Changed item reference debug

--- Assets/Scripts/ItemReference.cs
+++ Assets/Scripts/ItemReference.cs
@@ -5,21 +5,16 @@ public class ItemReference : MonoBehaviour
     public GameObject reference;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("poijhkugfc");
+        Debug.Log("TriggEnter");
         if (collision.TryGetComponent(out ItemPickUp itemPickUp))
         {
             itemPickUp.StumbleIntoItem(this);
         }
     }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log("caca");
-    }
-
     private void OnTriggerExit2D(Collider2D collision)
     {
-        Debug.Log("poiutydgsd");
+        Debug.Log("TriggExit");
         if (collision.TryGetComponent(out ItemPickUp itemPickUp))
         {
             itemPickUp.EscapeItemRange(this);
