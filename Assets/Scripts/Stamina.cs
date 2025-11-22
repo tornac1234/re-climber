8987865661f1cef57b208854bbd89307eabf238f
Matomagic <tomymarchant1@gmail.com>
2025-11-22 11:02:01 +0100

    Added stamina deplete function

--- Assets/Scripts/Stamina.cs
+++ Assets/Scripts/Stamina.cs
@@ -10,6 +10,28 @@ public class Stamina : MonoBehaviour
     public bool isRest = false;
     public UnityAction noStamina;
 
+    public void setStamina(float newValue)
+    {
+        /*
+         *  Set stamina value directly (in the range from 0. to 1.)
+         */
+        value = Mathf.Clamp(newValue, 0F, 1F);
+    }
+
+    public bool consumeStamina(float amount)
+    {
+        /*
+         * Consumes given amount of stamina, if possible.
+         * If it is possible, returns true, else returns false
+         */
+        if (value < amount)
+        {
+            return false;
+        }
+        setStamina(value - amount);
+        return true;
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -18,17 +40,17 @@ public class Stamina : MonoBehaviour
         bool isResting = false; // à relier à Movement.cs. Pour l'instant, on utilise isRest à la place (variable publique modifiable en live)
         if (isClimbing)
         {
-            value = Mathf.Clamp(value - dt / timeDeplete, 0F, 1F);
+            setStamina(value - dt / timeDeplete);
             if (value == 0F)
             {
-                noStamina.Invoke();
+                noStamina?.Invoke();
             }
         }
         else if (isRest)
         {
             if (value < 1.0)
             {
-                value = Mathf.Clamp(value + dt / timeRefill, 0F, 1F);
+                setStamina(value + dt / timeRefill);
             }
         }
     }
