37a6083ff9d5fcf5f06377e6523f879a453d0a20
Eragon1er <andili.mahieu@gmail.com>
2025-11-22 22:05:59 +0100

    Stamina bar
    Now changes 'smoothly'

--- Assets/Scripts/inGameUI.cs
+++ Assets/Scripts/inGameUI.cs
@@ -4,18 +4,40 @@ using UnityEngine.UI;
 public class inGameUI : MonoBehaviour
 {
     public float maxWidth = 750f;
+    public float aimWidth;
+    public float lastWidth;
+
+    public float timeToStam = 2f;
+    public float timeLasting;
 
     public Image staminaBar;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        aimWidth = maxWidth;
         maxWidth = staminaBar.GetComponent<RectTransform>().sizeDelta.x;
         staminaUpdate(0.2f);
     }
 
+
+    /*
+     * Call to this function to visually change the stamina bar
+     */
     public void staminaUpdate(float staminaRatio)
     {
-        staminaBar.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, staminaRatio * maxWidth);
+        aimWidth = staminaRatio * maxWidth;
+        timeLasting = 0f;
+        lastWidth = staminaBar.GetComponent<RectTransform>().sizeDelta.x;
+
+    }
+    
+    private void Update()
+    {
+        if (timeLasting < timeToStam)
+        {
+            timeLasting = timeLasting + Time.deltaTime;
+            staminaBar.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, Mathf.Lerp(lastWidth, aimWidth, timeLasting));
+        }
     }
 }
