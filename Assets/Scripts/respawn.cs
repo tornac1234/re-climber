e352707a4b2064e291081ae91ffc63969bc59263
yaacine224 <senouciyacine33@gmail.com>
2025-11-23 10:15:49 +0100

    respawn

--- Assets/Scripts/respawn.cs
+++ Assets/Scripts/respawn.cs
@@ -1,27 +1,34 @@
 using UnityEngine;
+using System.Generics;
 
 
 public class respawn : MonoBehaviour
 {
     public Stamina stamina;
-    public bool IsClimbing;
-    public bool IsGrounded;
+    public Climbing climbing;
+    public Movement movement;
     public float fallThreshold = -10f;
     public float falldistance;
+    public float deathCooldown = 3f;
     public Rigidbody2D rb;
-    void Start()
+    void Start()    
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        stamina = GetComponent<Stamina>();
     }
     void Update()
     {
         if (falldistance < fallThreshold)
         {
             Debug.Log("Respawn");
-            transform.position = new Vector3(0, 0, 0);
+            if (climbing.IsGrounded)
+            {
+                StartCoroutine(Respawn());
+
+            }
             falldistance = 0;
         }
-        if (IsGrounded)
+        if (climbing.IsGrounded)
         {
             falldistance = 0;
         }
@@ -29,11 +36,21 @@ public class respawn : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (!IsClimbing && !IsGrounded && stamina.value>0)
+        if (!climbing.IsClimbing && !movement.IsGrounded && stamina.value>0)
         {
             falldistance += Time.deltaTime*rb.linearVelocity.y; 
         
         }
                 
+    }
+    IEnumerator Respawn()   //Activer les aniimations correctes
+    {   
+        climbing.enabled = false;
+        movement.enabled = false;
+        yield return new WaitForSeconds(deathCooldown);
+        transform.position = new Vector3(0f, -2.5f, 0f);
+    
+        climbing.enabled = true;
+        movement.enabled = true;
     }
 }
