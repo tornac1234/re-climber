9c86d91b4c78a3aff9d3c17a252f4e1be5230424
rootcan <24827220+tornac1234@users.noreply.github.com>
2025-11-23 10:28:54 +0100

    fixed respawn for good

--- Assets/Scripts/respawn.cs
+++ Assets/Scripts/respawn.cs
@@ -7,47 +7,54 @@ public class respawn : MonoBehaviour
     public Stamina stamina;
     public Climbing climbing;
     public Movement movement;
-    public float fallThreshold = -10f;
-    public float falldistance;
+    public float fallThreshold = -3;
+    public float fallDistance;
     public float deathCooldown = 3f;
+    public bool isDead;
     public Rigidbody2D rb;
-    void Start()    
+
+    private void Start()    
     {
         rb = GetComponent<Rigidbody2D>();
         stamina = GetComponent<Stamina>();
     }
-    void Update()
-    {
-        if (falldistance < fallThreshold)
-        {
-            Debug.Log("Respawn");
-            if (movement.IsGrounded)
-            {
-                StartCoroutine(Respawn());
 
+    private void Update()
+    {
+        if (fallDistance < fallThreshold)
+        {
+            if (movement.IsGrounded && !isDead)
+            {
+                Debug.Log("Respawn");
+                StartCoroutine(Respawn());
+                fallDistance = 0;
             }
-            falldistance = 0;
         }
         if (movement.IsGrounded)
         {
-            falldistance = 0;
+            fallDistance = 0;
         }
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        if (!climbing.IsClimbing && !movement.IsGrounded && stamina.value>0)
+        if (!isDead && !climbing.IsClimbing && !movement.IsGrounded)
         {
-            falldistance += Time.deltaTime*rb.linearVelocity.y; 
+            fallDistance += Time.deltaTime*rb.linearVelocity.y; 
         }
     }
-    IEnumerator Respawn()   //Activer les aniimations correctes
-    {   
+
+    public IEnumerator Respawn()   //Activer les aniimations correctes
+    {
+        isDead = true;
         climbing.enabled = false;
         movement.enabled = false;
+
         yield return new WaitForSeconds(deathCooldown);
         transform.position = new Vector3(0f, -2.5f, 0f);
-    
+        fallDistance = 0;
+
+        isDead = false;
         climbing.enabled = true;
         movement.enabled = true;
     }
