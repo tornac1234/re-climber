b333a7968493127a9fb40e66623e0d30679fa743
yaacine224 <senouciyacine33@gmail.com>
2025-11-23 09:50:08 +0100

    respawn

--- Assets/Scripts/respawn.cs
+++ Assets/Scripts/respawn.cs
@@ -8,9 +8,23 @@ public class respawn : MonoBehaviour
     public bool IsGrounded;
     public float fallThreshold = -10f;
     public float falldistance;
+    public Rigidbody2D rb;
     void Start()
     {
         
+    }
+    void Update()
+    {
+        if (falldistance < fallThreshold)
+        {
+            Debug.Log("Respawn");
+            transform.position = new Vector3(0, 0, 0);
+            falldistance = 0;
+        }
+        if (IsGrounded)
+        {
+            falldistance = 0;
+        }
     }
 
     void FixedUpdate()
@@ -18,6 +32,7 @@ public class respawn : MonoBehaviour
         if (!IsClimbing && !IsGrounded && stamina.value>0)
         {
             falldistance += Time.deltaTime*rb.linearVelocity.y; 
+        
         }
                 
     }
